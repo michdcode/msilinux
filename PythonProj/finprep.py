f68834cced8ee0a7b5f445a1c0a4451be7e38ea9
michdcode <michd@DESKTOP-GNV4GB8.localdomain>
2020-10-17 15:04:50 -0700

    refactored classes added comments and docstring

--- PythonProj/finprep.py
+++ PythonProj/finprep.py
@@ -2,210 +2,201 @@ import itertools
 from datetime import date, datetime
 
 
-class Person():
-    """This is a generic class for any user or gift recipient."""
+class Person(object):
+    """Person class is a base class for users and friends."""
 
     # counter for assignment of UUID's, list of UUID's
     _UUID_counter = itertools.count()
     _UUID_lst = []
 
-    
-    def __init__(self, first="", middle="", last=""):
-        """This is the recipe for all Person objects"""
+    def __init__(self, first="", last=""):
+        """This constructor creates a person object."""
         self.first_name = first
-        self.middle_name = middle
         self.last_name = last
-        self.UUID = self.__set_UUID()
-        self._UUID_lst.append(self.UUID)
-        
-    
+        self._UUID = self.__set_UUID()
+        self._UUID_lst.append(self._UUID)
+
     def __set_UUID(self):
         """This private method assigns a UUID to each Person object."""
         self.new_UUID = next(self._UUID_counter)
         return self.new_UUID
 
-
     def __get_UUID_lst(self):
         """ This will return a list of all UUIDS"""
         return self._UUID_lst
-    
-    
-    def set_name_change(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-        return 'updated name.'
-
-        # to use: >>> michd.set_name_change(**{'first_name':'Michelle', 'last_name':'Dicks'})
-        # or for one key >>> michd.set_name_change(**{'first_Name': 'Michelle'})
-        # CAREFUL - when spelling keys it will create a new k:v pair if you misspell the key 
-        # need to make sure to use upper/lower, etc when setting and retrieving name
 
 
-    
 class User(Person):
-          
-    def __init__(self, first="", middle="", last=""):
-        Person.__init__(self, first, middle, last)
-        self._friend_lst = []
+    """User is subclass of Person, who is tracking gift ideas."""
 
-    def user_info(self, email='', pwd='', hint={}):
+    def __init__(self, first="", last="", email='', pwd='', hint={}):
+        """Creates a User object, which is a subclass of Person."""
+
+        Person.__init__(self, first, last)
         self.email = email
+        # in real life would not store password as plain text
         self.__password = pwd
         self.hint = hint
-        
+        self._friend_lst = []
 
-    def __set_new_pwd(self, new_pwd):
-        self.__password = new_pwd
-    
+    def set_updated_user(self, **kwargs):
+        """Allows updates to User attributes."""
+
+        # in real life would not update password this way
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        return 'updated user info.'
 
     def set_new_friend(self, friend):
-        self._friend_lst.append(friend)
-        return "added new friend to list"
-        # friend is a Friend object
+        """Allows user to add Friend object to friend list. """
 
+        self._friend_lst.append(friend)
+        return "Added new friend to list"
 
     def get_friend_lst(self):
+        """Returns the entire list of friends for a user."""
+
         return self._friend_lst
 
-
     def prnt_friend_lst(self):
-        print("You have {} friend(s). Here is a list:"\
-            .format(len(self._friend_lst)))
-        for friend in self._friend_lst:
-            print("Name: {} {} {}, Birthday: {}, Days to birthday: {}"\
-                .format(friend.first_name, friend.middle_name,\
-                        friend.last_name, friend.birthdatestr, 
-                        friend.days_to_birthdaystr))
-   
-   
-    def get_friend(self, first, middle, last):
+        """Prints friend list in readable format."""
+
+        print("You have {} friend(s). Here is a list:"
+              .format(len(self._friend_lst)))
+        for count, friend in enumerate(self._friend_lst, 1):
+            print(count, friend)
+
+    def get_friend(self, first, last):
+        """Finds friend on friend list by first and last name."""
+
         for friend in self._friend_lst:
             if (first == friend.first_name) and\
-               (middle == friend.middle_name) and\
                (last == friend.last_name):
-               return friend
+                return friend
         else:
             return "Friend not found."
 
-
     def __repr__(self):
-        return repr('Name: ' + self.first_name + ' ' + self.last_name \
+        """Returns offical representation of User object."""
+        return repr('Name: ' + self.first_name + ' ' + self.last_name
                     + ', email: ' + self.email)
-    
 
-    """
-    >>> bubba.set_new_pwd("puppies")
-    >>> bubba.password
-    #'puppies'
-    https://www.pythonanywhere.com 
-    >>> bubba = User("Bubba", "Jerome", "Wallace")
-    >>> bubba.UUID
-    2
-    >>> bubba.user_info("bubba@mail.com", "idjsos", {"What is your favorite number": 43})
-    >>> bubba.password
-    'idjsos'
-    >>> bubba.hint.values()
-    dict_values([43])
-    >>> bubba.hint['What is your favorite number']
-    43
-    >>> michelle = User("michelle", "therese", "dicks")
-    >>> annette = Friend("Annette", "Heller", "Lum")
-    >>> annette.friend_info('annlum@email.com', 4, 18, 1970)
-    >>> michelle.set_new_friend(annette)
-    added new friend to list
-    >>> michelle.get_friend_lst()
-    [<__main__.Friend object at 0x7fffbcdf4c18>]
-    >>> michelle.prnt_friend_lst()
-    Annette 1970-04-18
-    >>> michd.get_friend('Annette', 'Heller', "Lu")
-    'Friend not found.'
-    >>> michd.get_friend('Annette', 'Heller', "Lum")
-    'Name: Annette Lum, birthday: April 18, 1970, days to birthday: 176'
-    """
 
 class Friend(Person):
-    
-    def __init__(self, first="", middle="", last=""):
-        Person.__init__(self, first, middle, last)
-        self._gift_lst = []
+    """Friend is subclass of Person,potential gift recipient."""
 
-    def friend_info(self, email='', month=1, day = 1, year=2020):
-        # if a year is not available use this year's date
+    def __init__(self, first="", last="", email='', month=1, day=1,
+                 year=2020):
+        """Creates a Friend object, which is subclass of Person."""
+
+        Person.__init__(self, first, last)
         self.email = email
         self.birthdate = date(year=year, month=month, day=day)
-        self.birthdatestr = self.birthdate.strftime("%B %d, %Y") #? maybe this should be protected
+        self.birthdatestr = self.birthdate.strftime("%B %d, %Y")
         today = date.today()
         this_year_birthday = date(today.year, month, day)
         self.days_to_birthday = (abs(this_year_birthday - today)).days
         self.days_to_birthdaystr = str(self.days_to_birthday)
-         
+        self._gift_lst = []
 
-    def set_updated_friend_info(self, **kwargs):
+    def set_updated_friend(self, **kwargs):
+        """Allows updates to Friend attributes."""
+
         for k, v in kwargs.items():
             setattr(self, k, v)
         return 'updated friend info.'
 
-
     def set_new_gift_on_giftlist(self, gift):
+        """Allows user to add a Gift object to gift list for friend."""
+
         self._gift_lst.append(gift)
         return "added new gift to giftlist"
-        #gift is a Gift object
 
     def get_gift_lst(self):
+        """Returns the entire gift list of friend."""
+
         return self._gift_lst
 
-    
     def print_gift_lst(self):
+        """Prints gift list in readable format."""
+
         for count, gift in enumerate(self._gift_lst, 1):
             print(count, gift)
 
-
     def __repr__(self):
-        return repr('Name: ' + self.first_name + ' ' + self.last_name \
-                    + ', birthday: ' + self.birthdatestr \
-                    + ', days to birthday: ' + self.days_to_birthdaystr)
-    """
-        >>> annette = Friend("Annette", "Heller", "Lum")
-        >>> annette.friend_info('annlum@email.com', 4, 18, 1970)
-        >>> shoes = Gift('Cole Haan', 'https://www.colehaan.com', 'She likes ballet, these slippers are perfect')
-        >>> hat = Gift('Dodgers', 'https://www.dodgers.com', 'She and her husband like the Dodgers')
-        >>> annette.set_new_gift_on_giftlist(shoes)
-        'added new gift to giftlist'
-        >>> annette.set_new_gift_on_giftlist(hat)
-        'added new gift to giftlist'
-        >>> annette.print_gift_lst()
-        1 'Idea: Cole Haan, URL: https://www.colehaan.com, Notes: She likes ballet, these slippers are perfect'
-        2 'Idea: Dodgers, URL: https://www.dodgers.com, Notes: She and her husband like the Dodgers'
-        >>> repr(annette)
-        "'Name:Annette Lum, birthday: April/18/1970, daAys to birthday: 176'"
-    """
+        """Returns offical representation of Friend object."""
 
-    
+        return repr('Name: ' + self.first_name + ' ' + self.last_name
+                    + ', birthday: ' + self.birthdatestr
+                    + ', days to birthday: ' + self.days_to_birthdaystr)
+
+
 class Gift():
-    def __init__(self, name = '', URL='', notes=''):
+    """Generic class to store gift information."""
+
+    def __init__(self, name='', URL='', notes=''):
+        """Creates a gift object."""
+
         self.idea = name
         self.URL = URL
         self.notes = notes
         self.gift = {'Idea': self.idea, 'URL': self.URL, 'Notes': self.notes}
 
     def set_updated_gift_info(self, **kwargs):
+        """Allows update to Gift object."""
+
         for k, v in kwargs.items():
             setattr(self, k, v)
         return 'updated friend info.'
 
     def __repr__(self):
-        return repr('Idea: ' + self.idea + ', URL: ' + self.URL +\
+        return repr('Idea: ' + self.idea + ', URL: ' + self.URL +
                     ', Notes: ' + self.notes)
- 
- 
- 
+
     """
-        >>> shoes = Gift('Cole Haan', 'https://www.colehaan.com', 'She likes ballet, these slippers are perfect')
-        >>> repr(shoes)
-        "'Idea: Cole Haan, URL:https://stackoverflow.com/questions/1045344/how-do-you-create-an-incremental-id-in-a-python-class/54318273#54318273, Notes: I love these ballerina like slippers'"
-        >>> shoes.set_updated_gift_info(**{'idea': 'Nike'})
-        'updated friend info.'
-        >>> shoes.idea
-        'Nike'
-    
+    michelle = User("michelle", "dicks", "michd@gmail.com", "passme", {'great_song': 'doves_cry'})
+    michelle.set_updated_user(**{'first_name':'Michie', 'email':'michelle@gmail.com'})
+    >>> michelle.set_updated_user(**{'password': 'frenchie'})
+    'updated user info.'
+    >>> michelle.password
+    'frenchie'
+    Bubba = User("Bubba", "Wallace", "bubs@gmail.com", "drive it fast", {'favorite number': 43})
+    >>> Bubba.hint
+    {'favorite number': 43}
+    >>> Annette = Friend("Annette", "Lum", "annli@gmail.com", 4, 18, 1970)
+    >>> repr(Annette)
+    "'Name: Annette Lum, birthday: April 18, 1970, days to birthday: 182'"
+    >>> shoes = Gift('Cole Haan', 'https://www.colehaan.com', 'She likes ballet, these slippers are perfect')
+    >>> hat = Gift('Dodgers', 'https://www.dodgers.com', 'She and her husband like the Dodgers')
+    >>> Annette.set_new_gift_on_giftlist(shoes)
+    'added new gift to giftlist'
+    >>> Annette.set_new_gift_on_giftlist(hat)
+    'added new gift to giftlist'
+    >>> Peter = Friend("Peter", "Express", "peter@gmail.com", 6, 26, 1985)
+    >>> shoes = Gift('Nike', 'https://www.nike.com', 'He likes to workout')
+    >>> Peter.set_new_gift_on_giftlist(shoes)
+    'added new gift to giftlist'
+    >>> hat = Gift('Patroits', 'https://www.patriots.com', 'He loves new England')
+    >>> Peter.set_new_gift_on_giftlist(hat)
+    'added new gift to giftlist'
+    >>> Annette.print_gift_lst()
+    1 'Idea: Cole Haan, URL: https://www.colehaan.com, Notes: She likes ballet, these slippers are perfect'
+    2 'Idea: Dodgers, URL: https://www.dodgers.com, Notes: She and her husband like the Dodgers'
+    >>> Peter.print_gift_lst()
+    1 'Idea: Nike, URL: https://www.nike.com, Notes: He likes to workout'
+    2 'Idea: Patroits, URL: https://www.patriots.com, Notes: He loves new England'
+    >>> michelle.set_new_friend(Annette)
+    'Added new friend to list'
+    >>> michelle.set_new_friend(Alice)
+    'Added new friend to list'
+    >>> Bubba.set_new_friend(Peter)
+    'Added new friend to list'
+    >>> michelle.prnt_friend_lst()
+    You have 2 friend(s). Here is a list:
+    1 'Name: Annette Lum, birthday: April 18, 1970, days to birthday: 182'
+    2 'Name: Alice Meng, birthday: March 31, 1968, days to birthday: 200'
+    >>> Bubba.prnt_friend_lst()
+    You have 1 friend(s). Here is a list:
+    # CAREFUL - when spelling keys it will create a new k:v pair if you misspell the key
+    # need to make sure to use upper/lower, etc when setting and retrieving name
     """
